16fb4e5fcaf2c56298e8b95236ceafc6bc22c1ff
Alfan jibal akbar <jibalalfan08@gmail.com>
2025-11-21 21:51:08 +0800

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,14 @@
+# alfan jibal akbar (24241150)
+class Kendaraan:
+    def __init__(self, jenis, merk, tahun):
+        self.jenis = jenis
+        self.merk = merk
+        self.tahun = tahun
+
+    def info(self):
+        return f"Jenis: {self.jenis}, Merk: {self.merk}, Tahun: {self.tahun}"
+
+mobil = Kendaraan("Mobil", "avanza", 2020)
+print(mobil.info())
+motor = Kendaraan("Motor", "scoopy", 2019)
+print(motor.info())
